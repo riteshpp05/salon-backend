e6bb9a6616810fa631ee8be380e32e2c1885a008
Ritesh Patil <riteshpatil702811@gmail.com>
2026-06-28 20:52:48 +0530

    updates new

--- app/main.py
+++ app/main.py
@@ -122,8 +122,8 @@ app.add_middleware(
         "http://localhost:3000",
     ],
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allow_headers=["Content-Type", "Authorization"],
+    allow_methods=["*"],
+    allow_headers=["*"],
     expose_headers=["Content-Type", "Authorization"],
 )
 
